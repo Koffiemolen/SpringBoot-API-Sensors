45d4b14e13bf1886d82c8174978d688734dfa972
Koffiemolen <bastiaan.clement@gmail.com>
2021-05-15 21:00:23 +0200

    Configured classes for retrieval of data as an object.

--- temperature-retrieve-service/src/main/java/nl/coolpuntbeer/temperatureretrieveservice/models/Temperature.java
+++ temperature-retrieve-service/src/main/java/nl/coolpuntbeer/temperatureretrieveservice/models/Temperature.java
@@ -1,4 +1,37 @@
 package nl.coolpuntbeer.temperatureretrieveservice.models;
 
 public class Temperature {
+    private String sensorId;
+    private double ambientTemperature;
+    private double objectTemperature;
+
+    public Temperature(String sensorId, double ambientTemperature, double objectTemperature) {
+        this.sensorId = sensorId;
+        this.ambientTemperature = ambientTemperature;
+        this.objectTemperature = objectTemperature;
+    }
+
+    public String getSensorId() {
+        return sensorId;
+    }
+
+    public void setSensorId(String sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    public double getAmbientTemperature() {
+        return ambientTemperature;
+    }
+
+    public void setAmbientTemperature(double ambientTemperature) {
+        this.ambientTemperature = ambientTemperature;
+    }
+
+    public double getObjectTemperature() {
+        return objectTemperature;
+    }
+
+    public void setObjectTemperature(double objectTemperature) {
+        this.objectTemperature = objectTemperature;
+    }
 }
--- temperature-retrieve-service/src/main/java/nl/coolpuntbeer/temperatureretrieveservice/models/TemperatureSummary.java
+++ temperature-retrieve-service/src/main/java/nl/coolpuntbeer/temperatureretrieveservice/models/TemperatureSummary.java
@@ -1,4 +1,36 @@
 package nl.coolpuntbeer.temperatureretrieveservice.models;
 
+//    Example or returned JSON object
+//    ambientTemperature	"19.73"
+//    objectTemperature	"18.85"
+
 public class TemperatureSummary {
+
+    private String id;
+    private double ambientTemperature;
+    private double objectTemperature;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getAmbientTemperature() {
+        return ambientTemperature;
+    }
+
+    public void setAmbientTemperature(double ambientTemperature) {
+        this.ambientTemperature = ambientTemperature;
+    }
+
+    public double getObjectTemperature() {
+        return objectTemperature;
+    }
+
+    public void setObjectTemperature(double objectTemperature) {
+        this.objectTemperature = objectTemperature;
+    }
 }
