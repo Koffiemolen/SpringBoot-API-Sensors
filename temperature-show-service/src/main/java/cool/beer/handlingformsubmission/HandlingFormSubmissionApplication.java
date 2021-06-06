1d666dcc0279d45b9eb042b03e44ef73f8746a94
Koffiemolen <bastiaan.clement@gmail.com>
2021-06-06 12:33:47 +0200

    This service calls retrieve service with ID

---
+++ temperature-show-service/src/main/java/cool/beer/handlingformsubmission/HandlingFormSubmissionApplication.java
@@ -0,0 +1,20 @@
+package cool.beer.handlingformsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+
+public class HandlingFormSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlingFormSubmissionApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+}
